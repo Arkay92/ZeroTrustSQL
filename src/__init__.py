6a4d39e6398b5ec97bda27559b117f158be2dbba
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2024-09-12 07:54:45 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/__init__.py
+from .homomorphic_encryption import HomomorphicEncryption
+from .zero_knowledge_proof import ZKProof
+from .zk_database import ZKDatabase
+from .utils import str_to_int, int_to_str
